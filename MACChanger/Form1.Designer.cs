efbb8732ea84ac50820810f2ffbf73b28ef1666d
kuchen-msh <68498888+kuchen-msh@users.noreply.github.com>
2021-03-14 02:36:40 +0100

    Add files via upload

---
+++ MACChanger/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace MACChanger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nMAC = new System.Windows.Forms.TextBox();
+            this.cMAC = new System.Windows.Forms.Button();
+            this.aMAC = new System.Windows.Forms.TextBox();
+            this.State = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(40, 255);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Aktuelle MAC:";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(36, 305);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Neue MAC:";
+            // 
+            // nMAC
+            // 
+            this.nMAC.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.nMAC.Location = new System.Drawing.Point(39, 321);
+            this.nMAC.Name = "nMAC";
+            this.nMAC.Size = new System.Drawing.Size(205, 13);
+            this.nMAC.TabIndex = 3;
+            // 
+            // cMAC
+            // 
+            this.cMAC.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.cMAC.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cMAC.ForeColor = System.Drawing.Color.White;
+            this.cMAC.Location = new System.Drawing.Point(165, 378);
+            this.cMAC.Name = "cMAC";
+            this.cMAC.Size = new System.Drawing.Size(103, 23);
+            this.cMAC.TabIndex = 4;
+            this.cMAC.Text = "Wechsel die MAC";
+            this.cMAC.UseVisualStyleBackColor = false;
+            this.cMAC.Click += new System.EventHandler(this.cMAC_Click);
+            // 
+            // aMAC
+            // 
+            this.aMAC.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.aMAC.Location = new System.Drawing.Point(43, 271);
+            this.aMAC.Name = "aMAC";
+            this.aMAC.Size = new System.Drawing.Size(201, 13);
+            this.aMAC.TabIndex = 5;
+            // 
+            // State
+            // 
+            this.State.AutoSize = true;
+            this.State.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.State.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.State.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.State.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.State.ForeColor = System.Drawing.Color.Green;
+            this.State.Location = new System.Drawing.Point(43, 337);
+            this.State.Name = "State";
+            this.State.Size = new System.Drawing.Size(43, 15);
+            this.State.TabIndex = 6;
+            this.State.Text = "label3";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(0, -5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(343, 21);
+            this.panel1.TabIndex = 7;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(64, 8);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(157, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "MAC Changer by UnknownCPP";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(254, 8);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "X";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(43, 40);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(201, 212);
+            this.listBox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.ClientSize = new System.Drawing.Size(280, 413);
+            this.Controls.Add(this.cMAC);
+            this.Controls.Add(this.State);
+            this.Controls.Add(this.nMAC);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.aMAC);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "MACChanger";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox nMAC;
+        private System.Windows.Forms.Button cMAC;
+        private System.Windows.Forms.TextBox aMAC;
+        private System.Windows.Forms.Label State;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ MACChanger/Form1.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using pUnknownC;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace MACChanger
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+
+
+        private static extern IntPtr CreateRoundRectRgn
+         (
+              int nLeftRect,
+              int nTopRect,
+              int nRightRect,
+              int nBottomRect,
+              int nWidthEllipse,
+                 int nHeightEllipse
+
+          );
+        public Form1()
+        {
+            InitializeComponent();
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 15, 15));
+            aMAC.Text = _sysIn.GetMACAddress().ToString();
+            getNetworkAdapter();
+        }
+        
+        private Point _mouseMoving;
+        _mBoxes m = new _mBoxes();
+        RegistryKey r;
+        string key = String.Empty;
+        string[] result = new String[20];
+        
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cMAC_Click(object sender, EventArgs e)
+        {
+            string select = listBox1.SelectedItem.ToString();
+            
+            if(select == result[0])
+            {
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0001", true);
+                r.SetValue("NetworkAddress", nMAC.Text);
+                State.Visible = true;
+                State.Text = "MAC Adresse wurde geändert!";
+                r.Close();
+            }
+            if(select == result[1])
+            {
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0002", true);
+                r.SetValue("NetworkAddress", nMAC.Text);
+                State.Visible = true;
+                State.Text = "MAC Adresse wurde geändert!";
+                r.Close();
+            }
+            if(select == result[2])
+            {
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0003", true);
+                r.SetValue("NetworkAddress", nMAC.Text);
+                State.Visible = true;
+                State.Text = "MAC Adresse wurde geändert!";
+                r.Close();
+            }
+            if(select == result[3])
+            {
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0004", true);
+                r.SetValue("NetworkAddress", nMAC.Text);
+                State.Visible = true;
+                State.Text = "MAC Adresse wurde geändert!";
+                r.Close();
+            }
+            if (select == result[4])
+            {
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0005", true);
+                r.SetValue("NetworkAddress", nMAC.Text);
+                State.Visible = true;
+                State.Text = "MAC Adresse wurde geändert!";
+                r.Close();
+            }
+            if (select == result[5])
+            {
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0006", true);
+                r.SetValue("NetworkAddress", nMAC.Text);
+                State.Visible = true;
+                State.Text = "MAC Adresse wurde geändert!";
+                r.Close();
+            }
+            if (select == result[6])
+            {
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0007", true);
+                r.SetValue("NetworkAddress", nMAC.Text);
+                State.Visible = true;
+                State.Text = "MAC Adresse wurde geändert!";
+                r.Close();
+            }
+            if (select == result[7])
+            {
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0008", true);
+                r.SetValue("NetworkAddress", nMAC.Text);
+                State.Visible = true;
+                State.Text = "MAC Adresse wurde geändert!";
+                r.Close();
+            }
+            if (select == result[8])
+            {
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0009", true);
+                r.SetValue("NetworkAddress", nMAC.Text);
+                State.Visible = true;
+                State.Text = "MAC Adresse wurde geändert!";
+                r.Close();
+            }
+            if (select == result[9])
+            {
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0010", true);
+                r.SetValue("NetworkAddress", nMAC.Text);
+                State.Visible = true;
+                State.Text = "MAC Adresse wurde geändert!";
+                r.Close();
+            }
+            if (select == result[10])
+            {
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0011", true);
+                r.SetValue("NetworkAddress", nMAC.Text);
+                State.Visible = true;
+                State.Text = "MAC Adresse wurde geändert!";
+                r.Close();
+            }
+            if (select == result[11])
+            {
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0012", true);
+                r.SetValue("NetworkAddress", nMAC.Text);
+                State.Visible = true;
+                State.Text = "MAC Adresse wurde geändert!";
+                r.Close();
+            }
+            if (select == result[12])
+            {
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\00013", true);
+                r.SetValue("NetworkAddress", nMAC.Text);
+                State.Visible = true;
+                State.Text = "MAC Adresse wurde geändert!";
+                r.Close();
+            }
+            if (select == result[13])
+            {
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0014", true);
+                r.SetValue("NetworkAddress", nMAC.Text);
+                State.Visible = true;
+                State.Text = "MAC Adresse wurde geändert!";
+                r.Close();
+            }
+            if (select == result[14])
+            {
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0015", true);
+                r.SetValue("NetworkAddress", nMAC.Text);
+                State.Visible = true;
+                State.Text = "MAC Adresse wurde geändert!";
+                r.Close();
+            }
+            if (select == result[15])
+            {
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0016", true);
+                r.SetValue("NetworkAddress", nMAC.Text);
+                State.Visible = true;
+                State.Text = "MAC Adresse wurde geändert!";
+                r.Close();
+            }
+            if (select == result[16])
+            {
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0017", true);
+                r.SetValue("NetworkAddress", nMAC.Text);
+                State.Visible = true;
+                State.Text = "MAC Adresse wurde geändert!";
+                r.Close();
+            }
+            if (select == result[17])
+            {
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0018", true);
+                r.SetValue("NetworkAddress", nMAC.Text);
+                State.Visible = true;
+                State.Text = "MAC Adresse wurde geändert!";
+                r.Close();
+            }
+            if (select == result[18])
+            {
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0019", true);
+                r.SetValue("NetworkAddress", nMAC.Text);
+                State.Visible = true;
+                State.Text = "MAC Adresse wurde geändert!";
+                r.Close();
+            }
+            if (select == result[19])
+            {
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0020", true);
+                r.SetValue("NetworkAddress", nMAC.Text);
+                State.Visible = true;
+                State.Text = "MAC Adresse wurde geändert!";
+                r.Close();
+            }
+            
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Application.Exit();
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            _mouseMoving = e.Location;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                int x = e.Location.X - _mouseMoving.X;
+                int y = e.Location.Y - _mouseMoving.Y;
+                this.Location = new Point(this.Location.X + x, this.Location.Y + y);
+            }
+        }
+
+        private void getNetworkAdapter()
+        {
+            try
+            {
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0001", true);
+                result[0] = r.GetValue("DriverDesc").ToString();
+                listBox1.Items.Add(result[0]).ToString();
+
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0002", true);
+                result[1] = r.GetValue("DriverDesc").ToString();
+                listBox1.Items.Add(result[1]).ToString();
+
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0003", true);
+                result[2] = r.GetValue("DriverDesc").ToString();
+                listBox1.Items.Add(result[2]).ToString();
+
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0004", true);
+                result[3] = r.GetValue("DriverDesc").ToString();
+                listBox1.Items.Add(result[3]).ToString();
+
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0005", true);
+                result[4] = r.GetValue("DriverDesc").ToString();
+                listBox1.Items.Add(result[4]).ToString();
+
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0006", true);
+                result[5] = r.GetValue("DriverDesc").ToString();
+                listBox1.Items.Add(result[5]).ToString();
+
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0007", true);
+                result[6] = r.GetValue("DriverDesc").ToString();
+                listBox1.Items.Add(result[6]).ToString();
+
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0008", true);
+                result[7] = r.GetValue("DriverDesc").ToString();
+                listBox1.Items.Add(result[7]).ToString();
+
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0009", true);
+                result[8] = r.GetValue("DriverDesc").ToString();
+                listBox1.Items.Add(result[8]).ToString();
+
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0010", true);
+                result[9] = r.GetValue("DriverDesc").ToString();
+                listBox1.Items.Add(result[9]).ToString();
+
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0011", true);
+                result[10] = r.GetValue("DriverDesc").ToString();
+                listBox1.Items.Add(result[10]).ToString();
+
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0012", true);
+                result[11] = r.GetValue("DriverDesc").ToString();
+                listBox1.Items.Add(result[11]).ToString();
+
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0013", true);
+                result[12] = r.GetValue("DriverDesc").ToString();
+                listBox1.Items.Add(result[12]).ToString();
+
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0014", true);
+                result[13] = r.GetValue("DriverDesc").ToString();
+                listBox1.Items.Add(result[13]).ToString();
+
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0015", true);
+                result[14] = r.GetValue("DriverDesc").ToString();
+                listBox1.Items.Add(result[14]).ToString();
+
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0016", true);
+                result[15] = r.GetValue("DriverDesc").ToString();
+                listBox1.Items.Add(result[15]).ToString();
+
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0017", true);
+                result[16] = r.GetValue("DriverDesc").ToString();
+                listBox1.Items.Add(result[16]).ToString();
+
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0018", true);
+                result[17] = r.GetValue("DriverDesc").ToString();
+                listBox1.Items.Add(result[17]).ToString();
+
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0019", true);
+                result[18] = r.GetValue("DriverDesc").ToString();
+                listBox1.Items.Add(result[18]).ToString();
+
+                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}\\0020", true);
+                result[19] = r.GetValue("DriverDesc").ToString();
+                listBox1.Items.Add(result[19]).ToString();
+
+
+            }
+            catch
+            {
+
+            }
+        }
+
+       
+    }
+}
